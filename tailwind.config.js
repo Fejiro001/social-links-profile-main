3c6453b070caf32339abd40c6be7d0f3462b1933
Abere Fejiro <aberefejiro@gmail.com>
2024-03-06 23:58:41 +0100

    added resources to config file, and structured HTML

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,20 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-color": "hsl(75, 94%, 57%)",
+        white: "hsl(0, 0%, 100%)",
+        grey: "hsl(0, 0%, 20%)",
+        "dark-grey": "hsl(0, 0%, 12%)",
+        "off-black": "hsl(0, 0%, 8%)",
+      },
+
+      fontFamily: {
+        "primary-font": ["Inter", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
 
